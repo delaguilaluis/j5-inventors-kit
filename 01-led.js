96cd19f329571629157db8e2d162e2d6e8b0d1bf
Luis Del Águila <luisdelaguila@outlook.com>
2016-09-25 21:42:58 -0600

    Implement the first two experiments

---
+++ 01-led.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const Tessel = require('tessel-io');
+const five = require('johnny-five');
+
+const board = new five.Board({
+  io: new Tessel(),
+});
+
+board.on('ready', () => {
+  const led = new five.Led('a5');
+
+  // led.blink(500);
+  led.pulse(1000);
+});
---
+++ README.md
@@ -0,0 +1,3 @@
+# Experiment Guide for the Johnny-Five Inventor's Kit
+
+My implementation of [this guide](https://learn.sparkfun.com/tutorials/experiment-guide-for-the-johnny-five-inventors-kit).
